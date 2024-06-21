80895574e06494870b375a0088615ca160f22433
kevinpauneljohn <johnkevinpaunel@gmail.com>
2024-06-21 23:41:24 +0800

    get

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login',[\App\Http\Controllers\Api\LoginController::class,'authenticate'])->name('api.login');
+Route::get('/login',[\App\Http\Controllers\Api\LoginController::class,'authenticate'])->name('api.login');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
